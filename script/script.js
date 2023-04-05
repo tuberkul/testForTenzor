5138e39b5daa65c427ff5c63be3ecb10e7097ab8
tuberkul <52918187+tuberkul@users.noreply.github.com>
2023-04-05 14:42:08 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,22 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     // создаем массив в который войдут занятые и свободные поля, чтобы скрипт мог "видеть" поле.
-    const   fieldOfGame = [   
-                            '', '', '',
-                            '', '', '',
-                            '', '', ''
-                        ], // Массив основного игрового поля
-            man = document.querySelector('.human'), 
+    const   man = document.querySelector('.human'), 
             comp = document.querySelector('.ai'),
             startButton = document.querySelector('#startButton'),
             restartButton = document.querySelector('#restartButton'),
-            winnigMEssage = document.querySelector('.winning-message'),
+            winnigMessage = document.querySelector('.start-message'),
             cells = document.querySelectorAll('.cell'),
             numPool = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ], //массив полей для ходов
-            excludePool = []; //массив "занятых" полей
-            
-            let manValue, compValue, counter = 1
+            scoreManDiv  = document.querySelector('.score-in-message-man'),
+            scoreCompDiv  = document.querySelector('.score-in-message-comp'),
+            theWinner = document.querySelector('.theWinner');
 
-            
+
+            let manValue, 
+                compValue, 
+                counter = 1, 
+                scoreMan = 0, 
+                scoreAi = 0, 
+                itsFirstGame = true, 
+                fieldOfGame = [], // Массив основного игрового поля
+                excludePool = []; //массив "занятых" полей 
+
 
     //Функция определения рандомного числа от 1 до num                    
     function randomInt (num) {
@@ -26,6 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Функция хода компьютера
     function aiTurn(compValue) {
+        excludePool = [];
+        console.log(compValue);
         const filteredPool = []; //Создаем обнуленный отфильтрованный массив
         fieldOfGame.forEach((e, i) => {
             if (fieldOfGame[i] !== '') {
@@ -55,20 +61,40 @@ document.addEventListener('DOMContentLoaded', () => {
             
         
     }
+
     //начало игры
     function start() {
+        excludePool = [];
+        fieldOfGame = [   
+            '', '', '',
+            '', '', '',
+            '', '', ''
+        ]
         cells.forEach(e => {
-            e.addEventListener('click', (event) => {
-                
-                if (e.classList.contains('x') || e.classList.contains('circle')) {   //проверка на свободное поле
-                } else {
-                    e.classList.add(manValue); //Запись в выбранное поле в HTML
-                    fieldOfGame[+e.dataset.cell] = manValue; // Запись в игровое поле
-                    aiTurn(compValue); // Ход компьютера
-                }
-                
-            })
+            e.classList.remove('x')
+            e.classList.remove('circle')
         })
+        winnigMessage.classList.remove('show');
+        winnigMessage.classList.add('hide');
+        if (itsFirstGame) {
+            cells.forEach(e => {
+                e.addEventListener('click', (event) => {
+                    
+                    if (e.classList.contains('x') || e.classList.contains('circle')) {   //проверка на свободное поле
+                    } else {
+                        e.classList.add(manValue); //Запись в выбранное поле в HTML
+                        fieldOfGame[+e.dataset.cell] = manValue; // Запись в игровое поле
+                        if (winnerChek(compValue, manValue, fieldOfGame, true, false, true) === "man"){ // проверка на победу человека после хода
+                            return;
+                        }
+                        aiTurn(compValue); // Ход компьютера
+                        winnerChek(compValue, manValue, fieldOfGame, false, true, true) // проверка на победу компьюьтера после хода
+                    }
+                    
+                })
+            })
+        }
+        
         if (compValue === 'x') { // Если компьютер ходит первым
             aiTurn(compValue); // Ход компьютера
         }
@@ -82,16 +108,14 @@ document.addEventListener('DOMContentLoaded', () => {
             const arrForMan = Array.from(fieldOfGame); // Массив ("игровое поле") для анализа хода человека
             arrForAi[filteredPool[i]] = compValue; // Помещаем на "игровое поле" ход компьютера
             arrForMan[filteredPool[i]] = manValue; // Помещаем на "игровое поле" ход человека
-            if (winnerChek(compValue, manValue, arrForMan) === 'man') { // Проверяем победит ли человек при следующем ходе 
+            if (winnerChek(compValue, manValue, arrForMan, true, false, false) === 'man') { // Проверяем победит ли человек при следующем ходе 
                 posToMoveinManReview = filteredPool[i]; // Если победит, то то записываем позицию, на которую должен походить компьютер, чтобы не победил человек
                 winManCoef++; // Если победит, то увеличиваем переменную для будущего сравнения
-                
             } 
             
-            if (winnerChek(compValue, manValue, arrForAi) === 'comp') { // Проверяем победит ли компьютер при следующем ходе 
+            if (winnerChek(compValue, manValue, arrForAi, false, true, false) === 'comp') { // Проверяем победит ли компьютер при следующем ходе 
                 posToMoveinAiReview = filteredPool[i]; // Если победил, то то записываем позицию, на которую должен походить компьютер, чтобы выиграть
                 winAiCoef++; // Если победит, то увеличиваем переменную для будущего сравнения
-                
             } 
   
         }
@@ -123,58 +147,93 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     // Функция проверки победителя (к сожалению, не смог придумать более оптимизированной проверки)
-    function winnerChek(compValue, manValue, arr) {
+    function winnerChek(compValue, manValue, arr, manCheckin, aiCheckin, finalCheckin) {
+        let winConditions = [               // массив победных комбинаций
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+            [0, 4, 8], [2, 4, 6] // diagonal
+        ];
 
-    
-        if (arr[0] === compValue && arr[1] === compValue && arr[2] === compValue) { // Участок проверки победы компьютера 
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[3] === compValue && arr[4] === compValue && arr[5] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[6] === compValue && arr[7] === compValue && arr[8] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[0] === compValue && arr[4] === compValue && arr[8] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[2] === compValue && arr[4] === compValue && arr[6] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[0] === compValue && arr[3] === compValue && arr[6] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[1] === compValue && arr[4] === compValue && arr[7] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[2] === compValue && arr[5] === compValue && arr[8] === compValue) {
-            // console.log('Победил компьютер');
-            return 'comp';
-        } else if (arr[0] === manValue && arr[1] === manValue && arr[2] === manValue) { // Участок проверки победы человека 
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[3] === manValue && arr[4] === manValue && arr[5] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[6] === manValue && arr[7] === manValue && arr[8] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[0] === manValue && arr[4] === manValue && arr[8] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[2] === manValue && arr[4] === manValue && arr[6] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[0] === manValue && arr[3] === manValue && arr[6] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[1] === manValue && arr[4] === manValue && arr[7] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
-        } else if (arr[2] === manValue && arr[5] === manValue && arr[8] === manValue) {
-            // console.log('Победил человек');
-            return 'man';
+        if(manCheckin && !finalCheckin) { // условие победы человека в анализе следующего хода
+            for (let i = 0; i < winConditions.length; i++) {
+                let [a, b, c] = winConditions[i];
+                if (arr[a] === manValue &&
+                    arr[b] === manValue &&
+                    arr[c] === manValue) {
+                        return 'man';
+                }
+            }
         }
+        else if (aiCheckin && !finalCheckin){ // условие победы компьютера в анализе следующего хода
+            for (let i = 0; i < winConditions.length; i++) {
+                let [a, b, c] = winConditions[i];
+                if (arr[a] === compValue &&
+                    arr[b] === compValue &&
+                    arr[c] === compValue) {
+                        return 'comp';
+                }
+            }
+        }
+
+        if (manCheckin && finalCheckin) { // условие победы человека в финальной проверке
+            for (let i = 0; i < winConditions.length; i++) {
+                let [a, b, c] = winConditions[i];
+                if (arr[a] === manValue &&
+                    arr[b] === manValue &&
+                    arr[c] === manValue) {
+                        scoreMan++;
+                        itsFirstGame = false;
+                        if (itsFirstGame) {
+                            startButton.style.display="block";
+                        } else {
+                            restartButton.style.display="block"
+                        }
+                        scoreManDiv.innerHTML = scoreMan;
+                        winnigMessage.classList.remove('hide');
+                        winnigMessage.classList.add('show');
+                        theWinner.innerHTML = "Human"
+                        return "man"
+                }
+            }
+        } else if (aiCheckin && finalCheckin) { // условие победы компьютера в финальной проверке
+            for (let i = 0; i < winConditions.length; i++) {
+                let [a, b, c] = winConditions[i];
+                if (arr[a] === compValue &&
+                    arr[b] === compValue &&
+                    arr[c] === compValue) {
+                        scoreAi++;
+                        itsFirstGame = false;
+                        if (itsFirstGame) {
+                            startButton.style.display="block";
+                        } else {
+                            restartButton.style.display="block"
+                        }
+                        scoreCompDiv.innerHTML = scoreAi;
+                        winnigMessage.classList.remove('hide');
+                        winnigMessage.classList.add('show');
+                        theWinner.innerHTML = "Computer"
+                        return
+                } 
+            }
+        }
+        
+        if (finalCheckin) {
+            console.log(excludePool);
+            if (excludePool.length === 9) {
+                itsFirstGame = false;
+                if (itsFirstGame) {
+                    startButton.style.display="block";
+                } else {
+                    restartButton.style.display="block"
+                }
+                winnigMessage.classList.remove('hide');
+                winnigMessage.classList.add('show');
+                theWinner.innerHTML = "Draw"
+                return
+            }
+        }
+                        
+
     }
 
     // Кнопка старт
@@ -182,8 +241,6 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(e);
         startButton.style.display="none";
         restartButton.style.display="block";
-        winnigMEssage.classList.remove('show');
-        winnigMEssage.classList.add('hide');
         // Распределение первого хода (первыми ходят всегда крестики, поэтому распределение, кто чем ходит)
        if (randomInt(2) === 1) {
             man.innerHTML = `X`;
@@ -200,19 +257,22 @@ document.addEventListener('DOMContentLoaded', () => {
         start()
     });
 
-    // restartButton.addEventListener('click', e => {
-    //     if (manValue === 'x') {
-    //         man.innerHTML = 'O';
-    //         manValue = 'circle';
-    //         comp.innerHTML = 'X';
+    restartButton.addEventListener('click', e => {
+        if (manValue === 'x') {
+            man.innerHTML = 'O';
+            manValue = 'circle';
+            comp.innerHTML = 'X';
+            manValue = 'x';
             
-    //     } else {
-    //         man.innerHTML = 'X';
-    //         manValue = 'x';
-    //         comp.innerHTML = 'O';
-    //         compValue= 'circle';
-    //     }
-    // });
+        } else {
+            man.innerHTML = 'X';
+            manValue = 'x';
+            comp.innerHTML = 'O';
+            compValue= 'circle';
+        }
+
+        start();
+    });
     
 });
 
